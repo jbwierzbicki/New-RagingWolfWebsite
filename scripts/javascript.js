5754a35cb3aa3144534fb2173c364a70f176bbd2
Johnathan <jbwierzbicki@gmail.com>
2019-12-05 21:31:57 -0500

    Christmas Modal 1

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -36,3 +36,25 @@ function furtherInfo() {
     x.style.display = "none";
   }
 }
+
+/* Christmas Modal */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+christmasModal = function(){
+  modal.style.display = "block";
+  captionText.innerHTML = "Happy Holidays from the PACK at Raging Wolf Solutions!";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
