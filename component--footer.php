d939ac43567474c4222527b7fecdcb10fc408210
Johnathan <jbwierzbicki@gmail.com>
2021-01-27 10:48:00 -0500

    added Leadfeeder script

--- component--footer.php
+++ component--footer.php
@@ -39,4 +39,22 @@
           </div>
         </div>
       </div>
+  <script>
+  (function(){
+
+    window.ldfdr = window.ldfdr || {};
+    (function(d, s, ss, fs){
+      fs = d.getElementsByTagName(s)[0];
+
+      function ce(src){
+        var cs  = d.createElement(s);
+        cs.src = src;
+        setTimeout(function(){fs.parentNode.insertBefore(cs,fs)}, 1);
+      }
+
+      ce(ss);
+    })(document, 'script', 'https://sc.lfeeder.com/lftracker_v1_kn9Eq4R1Yyr7RlvP.js');
+  })();
+</script>
+
     </footer>
