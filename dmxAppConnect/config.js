438fef8961e3c6a997945c13ca8ef7ce47a46a75
Johnathan <jbwierzbicki@gmail.com>
2021-04-29 14:24:41 -0400

    compressed banner modal on homepage for improved performance

--- dmxAppConnect/config.js
+++ dmxAppConnect/config.js
@@ -43,6 +43,118 @@ dmx.config({
         "type": "object",
         "name": "headers"
       }
+    ],
+    "trackingapi": [
+      {
+        "type": "array",
+        "name": "data",
+        "sub": [
+          {
+            "type": "object",
+            "name": "attributes",
+            "sub": [
+              {
+                "type": "text",
+                "name": "type"
+              },
+              {
+                "type": "text",
+                "name": "url"
+              }
+            ]
+          },
+          {
+            "type": "text",
+            "name": "Carrier_Name__c"
+          },
+          {
+            "type": "text",
+            "name": "Name"
+          },
+          {
+            "type": "text",
+            "name": "Load_Reference__c"
+          },
+          {
+            "type": "text",
+            "name": "Customer_Name__c"
+          },
+          {
+            "type": "number",
+            "name": "Total_Customer_Amount__c"
+          },
+          {
+            "type": "text",
+            "name": "FreightTM__Carrier_Obj__c"
+          },
+          {
+            "type": "number",
+            "name": "Total_Carrier_Amount__c"
+          },
+          {
+            "type": "text",
+            "name": "CreatedDate"
+          },
+          {
+            "type": "text",
+            "name": "FreightTM__Status__c"
+          },
+          {
+            "type": "text",
+            "name": "Pickup_Arrival_Date__c"
+          },
+          {
+            "type": "text",
+            "name": "FreightTM__Pickup_Address__c"
+          },
+          {
+            "type": "text",
+            "name": "FreightTM__Delivery_Address__c"
+          },
+          {
+            "type": "text",
+            "name": "Delivery_Arrival_Date__c"
+          },
+          {
+            "type": "text",
+            "name": "Id"
+          }
+        ]
+      },
+      {
+        "type": "object",
+        "name": "headers",
+        "sub": [
+          {
+            "type": "text",
+            "name": "date"
+          },
+          {
+            "type": "text",
+            "name": "cache-control"
+          },
+          {
+            "type": "text",
+            "name": "vary"
+          },
+          {
+            "type": "text",
+            "name": "strict-transport-security"
+          },
+          {
+            "type": "text",
+            "name": "content-encoding"
+          },
+          {
+            "type": "text",
+            "name": "transfer-encoding"
+          },
+          {
+            "type": "text",
+            "name": "content-type"
+          }
+        ]
+      }
     ]
   }
 });
