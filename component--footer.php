e897dfe9b05c17670c8ad4d1e3e1c7c44936490d
Johnathan <jbwierzbicki@gmail.com>
2022-03-20 13:44:52 -0400

    footer font updates

--- component--footer.php
+++ component--footer.php
@@ -3,11 +3,11 @@
       <div class="container wappler-block univ--footer--container">
         <div class="row univ--footer--container--row">
           <div class="col-12 col-sm-6 col-md-6 univ--footer--container--row--col">
-            <h6 class="univ--footer--container--row--col--h6 text-white mb-2 pb-1">Raging Wolf Solutions</h6>
+            <h6 class="text-white mb-2 pb-1">Raging Wolf Solutions</h6>
             <p class="text-justify mb-4">Freight transportation and logistics coordination done right. We deal with air/ground expedites, full truckloads, partials, cargo vans, sprinter vans and straight trucks all across the country. Here, you can view the full list of our services, request a quote, or track a shipment!</p>
           </div>
           <div class="col-12 col-sm-6 col-md-3 univ--footer--container--row--col2">
-            <h6 class="univ--footer--container--row--col--h6 text-white mb-2 pb-1">Services</h6>
+            <h6 class="text-white mb-2 pb-1">Services</h6>
             <ul class="list-group mb-3 pb-1">
             <a href="https://ragingwolfsolutions.com/services-expedited-ground"><li class="list-group-item bg-transparent univ--footer--container--row--col--li border-0 pt-0 pb-0 pl-0 pr-0">Expedited Ground</li></a>
             <a href="https://ragingwolfsolutions.com/services-trucking"><li class="list-group-item bg-transparent univ--footer--container--row--col--li border-0 pt-0 pb-0 pl-0 pr-0">Trucking</li></a>
@@ -16,7 +16,7 @@
             </ul>
           </div>
           <div class="col-12 col-sm-6 col-md-3 univ--footer--container--row--col3">
-            <h6 class="univ--footer--container--row--col--h6 text-white mb-2 pb-1">Quick Links</h6>
+            <h6 class="text-white mb-2 pb-1">Quick Links</h6>
             <ul class="list-group mb-3 pb-1">
               <a href="https://ragingwolfsolutions.com/"><li class="list-group-item bg-transparent univ--footer--container--row--col--li border-0 pt-0 pb-0 pl-0 pr-0">Home</li></a>
               <a href="https://ragingwolfsolutions.com/alphatrack"><li class="list-group-item bg-transparent univ--footer--container--row--col--li border-0 pt-0 pb-0 pl-0 pr-0">Track a Shipment</li></a>
