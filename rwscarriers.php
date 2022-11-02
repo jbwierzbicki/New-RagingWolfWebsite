3bb721af8aa2612a6659db53c1f6e18b146c114b
Johnathan <jbwierzbicki@gmail.com>
2022-11-02 10:01:58 -0400

    updated carrier packet 11-2-2022

--- rwscarriers.php
+++ rwscarriers.php
@@ -1,61 +1,67 @@
 <!doctype html>
-<html><head>
-<meta charset="UTF-8">
+<html>
+
+<head>
+  <meta charset="UTF-8">
   <title>Carriers | Raging Wolf Solutions</title>
   <script src="dmxAppConnect/dmxAppConnect.js"></script>
-<link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
+  <link href="https://fonts.cdnfonts.com/css/sansation" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="dmxAppConnect/dmxRouting/dmxRouting.js" defer=""></script>
   <script src="dmxAppConnect/dmxBootstrap4Navigation/dmxBootstrap4Navigation.js" defer=""></script>
-    <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
   <link href="https://fonts.googleapis.com/css2?family=Lustria&amp;family=Muli:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
   <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
-<link rel="manifest" href="icons/site.webmanifest">
-<link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#c4231c">
-<link rel="shortcut icon" href="icons/favicon.ico">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-config" content="/icons/browserconfig.xml">
-<meta name="theme-color" content="#ffffff">
+  <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
+  <link rel="manifest" href="icons/site.webmanifest">
+  <link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#c4231c">
+  <link rel="shortcut icon" href="icons/favicon.ico">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-config" content="/icons/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
   <script src="dmxAppConnect/dmxBootstrap4Alert/dmxBootstrap4Alert.js" defer=""></script>
   <link rel="stylesheet" href="dmxAppConnect/dmxValidator/dmxValidator.css">
   <script src="dmxAppConnect/dmxValidator/dmxValidator.js" defer=""></script>
   <script src="dmxAppConnect/dmxBootstrap4Modal/dmxBootstrap4Modal.js" defer=""></script>
   <meta name="description" content="Join our base of trusted partner carriers. The 'PACK' is moving freight all across the United States and Canada!">
   <script src="dmxAppConnect/bs-custom-file-input/bs-custom-file-input.min.js" defer=""></script>
-  </head>
-  <body is="dmx-app" id="index">
-    <?php include 'component--header.php';?>
-    <main class="contact--main">
-      <section class="contact--main--section">
-        <div class="contact--main--section--container h-100 container">
-          <div class="row justify-content-center row-cols-1 w-100 h-100 align-items-center">
-            <div class="pb-4 pl-1 pr-1 col-12 col-md-10">
-              <h5 class="text-uppercase text-yellow">carrier connection</h5>
-              <h1 class="text-light text-left">Interested in partnering with the PACK?</h1>
-              <p class="text-white-50 mb-4" style="max-width: 770px !important">Raging Wolf Solutions is always looking to expand its base of trusted, dedicated freight carrier partners. If you'd like to move freight with us, please begin by downloading the PDF below.</p>
-              <form id="carrierform">
-                <div class="row">
-                  <div class="text-left col-12">
-                    <h5 class="text-light">1. Save a copy of our carrier packet.</h5>
-                    <a target="_blank" rel="noopener noreferrer" href="assets/Updated%20Raging%20Wolf%20Carrier%20Packet%2008-30-2022%282%29.pdf">
-                      <button id="btn1" class="btn btn-secondary mb-4 pt-3 pb-3 pl-3 pr-3"><i class="fa fa-download"></i>&nbsp;Download Carrier Packet</button>
-                    </a>
-                    <h5 class="text-light pt-2">2. Send the completed carrier packet to us.</h5>
-                    <h5 class="text-primary text-truncate"><a href="mailto:support@ragingwolfsolutions.com" class="text-white">support@ragingwolfsolutions.com</a></h5>
-                  </div>
+</head>
+
+<body is="dmx-app" id="index">
+  <?php include 'component--header.php';?>
+  <main class="contact--main">
+    <section class="contact--main--section">
+      <div class="contact--main--section--container h-100 container">
+        <div class="row justify-content-center row-cols-1 w-100 h-100 align-items-center">
+          <div class="pb-4 pl-1 pr-1 col-12 col-md-10">
+            <h5 class="text-uppercase text-yellow">carrier connection</h5>
+            <h1 class="text-light text-left">Interested in partnering with the PACK?</h1>
+            <p class="text-white-50 mb-4" style="max-width: 770px !important">Raging Wolf Solutions is always looking to expand its base of trusted, dedicated freight carrier partners. If you'd like to move freight with us, please begin by downloading the PDF below.</p>
+            <form id="carrierform">
+              <div class="row">
+                <div class="text-left col-12">
+                  <h5 class="text-light">1. Save a copy of our carrier packet.</h5>
+                  <a target="_blank" rel="noopener noreferrer" href="assets/Raging%20Wolf%20Carrier%20Packet%20110122%284%29.pdf">
+                    <button id="btn1" class="btn btn-secondary mb-4 pt-3 pb-3 pl-3 pr-3"><i class="fa fa-download"></i>&nbsp;Download Carrier Packet</button>
+                  </a>
+                  <h5 class="text-light pt-2">2. Send the completed carrier packet to us.</h5>
+                  <h5 class="text-primary text-truncate"><a href="mailto:support@ragingwolfsolutions.com" class="text-white">support@ragingwolfsolutions.com</a></h5>
                 </div>
-              </form></div>
+              </div>
+            </form>
           </div>
         </div>
-      </section>
-    </main>
-    <?php include 'component--footer.php';?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="javascript/startquote.js"></script>
-  </body></html>
+      </div>
+    </section>
+  </main>
+  <?php include 'component--footer.php';?>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="javascript/startquote.js"></script>
+</body>
+
+</html>
